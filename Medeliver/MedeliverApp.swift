0d117884c25d00f8e5bd65fdd43f72e35be7c00f
peihan0 <ty9722218@gmail.com>
2023-12-16 20:05:58 +0800

    Initial Commit

---
+++ Medeliver/MedeliverApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedeliverApp.swift
+//  Medeliver
+//
+//  Created by Peihan Jiang on 2023/12/16.
+//
+
+import SwiftUI
+
+@main
+struct MedeliverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
